a604844202fbaf4e6b122c4e760be635bfa51c5a
AndreyKolodiy <mr.andreykolodiy@gmail.com>
2018-10-09 20:36:50 +0300

    My first commit

---
+++ lesson_2/andrey.kolodiy/src/MyFirstJavaApp.java
@@ -0,0 +1,8 @@
+class MyFirstJavaApp{
+    public static void main(String[] params){
+
+        System.out.println("Hello Andrey " + params[0] + " " + params[1]);
+    }
+}
+
+
--- lesson_2/ruslan.gramatic/src/MyFirstJavaApp.java
+++
@@ -1,5 +0,0 @@
-public class MyFirstJavaApp {
-  public static void main(String[] params) {
-  	System.out.println("Hello Ruslan " + params[1] + " ----- " + params[0] + "!!!!!!!");
-  }
-}
