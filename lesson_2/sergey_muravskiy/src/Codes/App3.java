4686d7b48b5ab9ad59c91f2bd0c79becd5eb57f8
Sergey <Deltr28@gmail.com>
2018-10-09 19:43:14 +0300

    First Sources

---
+++ lesson_2/sergey_muravskiy/src/Codes/App3.java
@@ -0,0 +1,25 @@
+class App3
+{
+    public static void main(String[] params) 
+	{
+		int Count_param = params.length;
+		float Min = Float.parseFloat(params[0]);
+		
+		for(int i=1;i<Count_param;i++)
+		{
+			float Params_value = 0;
+		    Params_value = Float.parseFloat(params[i]);
+			if (Params_value < Min) {Min = Params_value;}
+		}
+		
+		for(int i=0;i<Count_param;i++)
+		{
+			float Check = 0;
+			Check = Float.parseFloat(params[i]);
+			if(Check == Min) {System.out.println("Min" +' '+ ">" + ' ' + Min);}
+			
+		}
+    }
+}  
+
+//3) Найти минимальное из 3х чисел, причем, если минимальных несколько вывести все такие числа
