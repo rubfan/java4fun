ad443074566f812995d7d6267e6d649985aa3671
AndreyKolodiy <mr.andreykolodiy@gmail.com>
2018-10-10 17:10:13 +0300

    Task_5

---
+++ lesson_2/andrey.kolodiy/src/NoResidue.java
@@ -0,0 +1,23 @@
+/*
+
+Task 5
+
+Given the 2 numbers, determine whether the first number is divisible by the second without a remainder.
+
+*/
+
+class NoResidue{
+
+    public static void main(String[] params){
+
+        int num1 = Integer.parseInt(params[0]);
+        int num2 = Integer.parseInt(params[1]);
+        if (num1 % num2 == 0) {
+            System.out.println("No residue");
+        } else {
+            System.out.println("There is a residue");
+        }
+    }
+}
+
+
