f50c8d7513f3dbe142ade8d07fe0f041132dab7e
Daria Kram <itsokaytobegeek@gmail.com>
2018-10-11 17:00:50 +0300

    I messed everything up.

--- lesson_2/Daria.K/src/HomeWork_4.java
+++
@@ -1,7 +0,0 @@
-public class HomeWork_4 {
-	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		if (a%2 == 0) System.out.println("This number is even");
-			else System.out.println("This number is odd");
-	}
-}
