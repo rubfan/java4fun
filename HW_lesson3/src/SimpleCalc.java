3c83e9948ae8c094060344598d70705a513eb904
Temrad <artemiy.radiyk@gmail.com>
2018-10-14 18:20:37 +0300

    Second task completed

--- HW_lesson3/src/SimpleCalc.java
+++ HW_lesson3/src/SimpleCalc.java
@@ -1,12 +1,32 @@
+
+import java.util.Scanner;
+
 public class SimpleCalc {
     public static void main(String[] args) {
-        System.out.println("5 + 12 = "  +calculate("+", 5, 12));
-        System.out.println("5 - 12 = "  +calculate("-", 5, 12));
-        System.out.println("5 * 12 = "  +calculate("*", 5, 12));
-        System.out.println("12 / 5 = "  +calculate("/", 12, 5));
-        System.out.println("12 \\ 5 = " +calculate("\\", 12, 5));
-        System.out.println("12 % 5 = "  +calculate("%", 12, 5));
-        System.out.println("abs -15 = " +calculate("abs", -15, 0));
+
+        Scanner in = new Scanner(System.in);
+
+        boolean more = true;
+        while(more){
+
+            System.out.println("Input the operation please (+,-,*,/,%,abs):");
+            String operation = in.nextLine();
+
+            System.out.println("Input the first argument (integer):");
+            int arg1 = in.nextInt();
+
+            System.out.println("Input the second argument (integer):");
+            int arg2 = in.nextInt();
+
+            System.out.println("Result: " + calculate(operation, arg1, arg2));
+
+            System.out.println("Do you want to continue? [Y/n]");
+
+            // to consume an extra new-line character:
+            in.nextLine();
+
+            more = in.nextLine().matches("[Yy]");
+        }
     }
 
     private static int calculate(String operation, int arg1, int arg2){
