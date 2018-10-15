1265902425c3ea6767d7f146c55266fc6850a1b5
StasBakryu <stasbakryu@gmail.com>
2018-10-15 18:53:49 +0300

    hw 1

---
+++ lesson_2/stas.bakryu/src/HW1.java
@@ -0,0 +1,21 @@
+
+
+public class HW1 {
+
+    public static void main(String[] params) {
+         int a = Integer.parseInt(params[0]);
+        int b = Integer.parseInt(params[1]);
+        int c = Integer.parseInt(params[2]);
+        int min = whoIsBiger(a,b,c);
+
+        System.out.print("this number is min"+min);
+
+    }
+    public static int whoIsBiger(int a,int b, int c){
+        int min = a;
+        if (min > b) min = b;
+        if (min > c) min = c;
+        return min;
+    }
+
+}
