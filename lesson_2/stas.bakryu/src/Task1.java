491c7a3c0ec51428ef14835063b57f53a277a6fa
StasBakryu <stasbakryu@gmail.com>
2018-10-16 01:12:43 +0300

    Task3 commin))

--- lesson_2/stas.bakryu/src/Task1.java
+++ lesson_2/stas.bakryu/src/Task1.java
@@ -3,15 +3,16 @@
 public class Task1 {
 
     public static void main(String[] params) {
-         int a = Integer.parseInt(params[0]);
+        int a = Integer.parseInt(params[0]);
         int b = Integer.parseInt(params[1]);
         int c = Integer.parseInt(params[2]);
-        int min = whoIsBiger(a,b,c);
+        int min = whoIsBiger(a, b, c);
 
-        System.out.print("this number is min"+min);
+        System.out.print("this number is min" + min);
 
     }
-    public static int whoIsBiger(int a,int b, int c){
+
+    public static int whoIsBiger(int a, int b, int c) {
         int min = a;
         if (min > b) min = b;
         if (min > c) min = c;
