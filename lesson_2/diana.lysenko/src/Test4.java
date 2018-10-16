672523da0d7c05d6d403e51979da3c6cac372870
DianaLysenko <42473300+Liss091@users.noreply.github.com>
2018-10-16 19:22:42 +0300

    Old files

--- lesson_2/diana.lysenko/src/Test4.java
+++
@@ -1,8 +0,0 @@
-public class Test4 {
-public static void main(String[] params) {
-        int a = Integer.parseInt(params[0]);
-        if (a % 2 == 0) {
-            System.out.println("Число " + a + " четное.");
-        } else System.out.println("Число " + a + " нечетное.");
-    }
-}
--- lesson_2/diana.lysenko/src/Test5.java
+++
@@ -1,9 +0,0 @@
-public class Test5 {
-public static void main(String[] params) {
-        int a = Integer.parseInt(params[0]);
-        int b = Integer.parseInt(params[1]);
-        if (a % b == 0) {
-            System.out.println("Число " + a + " делится на число " + b + " без остатка.");
-        } else System.out.println("Число " + a + " не делится на число " + b + " без остатка.");
-    }
-}
