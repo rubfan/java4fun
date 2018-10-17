3452e2dae16c42039fba2e9a95fc438ffc8defb7
Sergey <Deltr28@gmail.com>
2018-10-18 00:53:52 +0300

    HWLesson3

---
+++ lesson_3/sergey.muravskiy/src/Tip.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.String;
+
+public class Tip {
+
+    public static void main(String[] args) {
+	
+        double Bill,Tea,Tip,TotalBill;
+        String QualityOfService="";
+        Scanner in = new Scanner(System.in);
+        System.out.print("Bill > ");
+        Bill = in.nextDouble();
+        System.out.print("Enter service quality [Terrible/Poor/Good/Great/Excelant] > ");
+        QualityOfService = in.next();
+        TotalBill = Bill;
+		//Качество обслуживания
+		switch(QualityOfService)
+            {
+                case "Terrible" :
+                    Tip = (Bill * 0)/100;
+                    break;
+                case "Poor" :
+                    Tip = (Bill * 5)/100;
+                    break;
+                case "Good" :
+                    Tip = (Bill *10)/100;
+                    break;
+				case "Great" :
+                    Tip = (Bill * 15)/100;
+                    break;
+				case "Excelant" :
+                    Tip = (Bill * 20)/100; 
+                    break;
+                default :
+                    Tip = 0;
+                    break;
+            }
+        //Вывод чаевых и счета с чаевыми
+		TotalBill +=Tip;
+        System.out.println("Tip > " + Tip + " UAH");
+		System.out.print("Bill with Tip > " + TotalBill  + " UAH");
+
+    }
+}
