4995466c0e6f1f6a802426dfded8147421762370
Sergey <Deltr28@gmail.com>
2018-10-18 01:58:11 +0300

    Allright

--- lesson_3/sergey.muravskiy/src/Tip.java
+++ lesson_3/sergey.muravskiy/src/Tip.java
@@ -13,7 +13,6 @@ public class Tip
         System.out.print("Enter service quality [Terrible/Poor/Good/Great/Excelant] > ");
         QualityOfService = in.next();
         TotalBill = Bill;
-<<<<<<< HEAD
         //Качество обслуживания
         switch (QualityOfService)
         {
@@ -40,34 +39,5 @@ public class Tip
         TotalBill += Tip;
         System.out.println("Tip > " + Tip + " UAH");
         System.out.print("Bill with Tip > " + TotalBill + " UAH");
-=======
-	//Качество обслуживания
-	switch(QualityOfService)
-            {
-                case "Terrible" :
-                    Tip = (Bill * 0)/100;
-                    break;
-                case "Poor" :
-                    Tip = (Bill * 5)/100;
-                    break;
-                case "Good" :
-                    Tip = (Bill *10)/100;
-                    break;
-		case "Great" :
-                    Tip = (Bill * 15)/100;
-                    break;
-		case "Excelant" :
-                    Tip = (Bill * 20)/100; 
-                    break;
-                default :
-                    Tip = 0;
-                    break;
-            }
-        //Вывод чаевых и счета с чаевыми
-	TotalBill +=Tip;
-        System.out.println("Tip > " + Tip + " UAH");
-	System.out.print("Bill with Tip > " + TotalBill  + " UAH");
-
->>>>>>> 2b40575a50f2a51bb3e3c0ea775173d5fb922964
     }
 }
