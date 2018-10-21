3d3f9a4da8171144deb8d8db1fd7aa8885b758d5
Vladimir <deadlight967@gmail.com>
2018-10-22 00:11:29 +0300

    Homework 4 VP

---
+++ lesson_4/VladimirPleHW4/Exercise1.java
@@ -0,0 +1,11 @@
+package com.company.VladimirPleHW4;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Integer a = new Integer(1000);
+        while (a<10000){
+            System.out.println(a);
+            a+=3;
+        }
+    }
+}
