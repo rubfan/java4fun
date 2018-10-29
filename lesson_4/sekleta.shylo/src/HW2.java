fc8a98fa3479de0cad23400b6448a08d1b710958
Sekleta <sekleta.shylo@gmail.com>
2018-10-29 11:32:04 +0200

    HomeWorkLesson4(пока неполный)

---
+++ lesson_4/sekleta.shylo/src/HW2.java
@@ -0,0 +1,13 @@
+public class HW2 {
+
+    public static void main(String[] args) {
+        int count = 1;
+        int a = -1;
+
+        while (count <= 55) {
+            a = a + 2;
+            System.out.print(a + " ");
+            count++;
+        }
+    }
+}
